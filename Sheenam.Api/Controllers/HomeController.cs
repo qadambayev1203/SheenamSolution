ae2f02bddcd0cdfc0166483205265ea6ab4bddf3
qadambayev1203 <qadamboyevmominjon003@gmail.com>
2024-08-06 10:22:13 +0500

    INFRA: Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [Route("api/home-controller")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet("get")]
+        public ActionResult<string> Get() => Ok("Hello Dear");
+    }
+}
