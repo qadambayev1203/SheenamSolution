2337f07606ca68769588345a26943c8f1a4f3cb0
qadambayev1203 <qadamboyevmominjon003@gmail.com>
2024-08-08 14:19:14 +0500

    CONFIG: Register Broker

--- Sheenam.Api/Program.cs
+++ Sheenam.Api/Program.cs
@@ -3,11 +3,14 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using Sheenam.Api.Brokers.Storages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
 
 var app = builder.Build();
 
