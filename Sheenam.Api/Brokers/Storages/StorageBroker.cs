24795aec30e1607ef746bdea14c45b93e53aef38
qadambayev1203 <qadamboyevmominjon003@gmail.com>
2024-08-08 14:18:06 +0500

    BROKERS: Insert Guest

--- Sheenam.Api/Brokers/Storages/StorageBroker.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.cs
@@ -9,7 +9,7 @@ using System.Configuration;
 
 namespace Sheenam.Api.Brokers.Storages
 {
-    public partial class StorageBroker : EFxceptionsContext
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
     {
         private readonly IConfiguration configuration;
 
